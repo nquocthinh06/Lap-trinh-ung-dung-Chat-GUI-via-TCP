3808ad45fee12ed8dc3011189b670297c4ea2988
NguyenThanhDuoc1 <nguyenthanhduoc8406@gmail.com>
2026-04-04 13:48:06 +0700

    Delete Code/Protocol.h

--- Code/Protocol.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-extern const int PORT;
-extern const string ENCODING;
-extern const string ENDLINE;
-string formatMessage(const std::string& username, const std::string& message);
